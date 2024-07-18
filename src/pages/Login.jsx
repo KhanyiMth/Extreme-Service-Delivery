fb11b9d88c343ede941909ec705c6cca71fdd556
KhanyiMth <139657158+KhanyiMth@users.noreply.github.com>
2024-07-18 14:04:56 +0200

    Add files via upload

---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEnvelope, faLock, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+import img1 from '../assets/signup.png';
+import '../scss/main.scss'
+
+function Login(){
+    return(
+        <>
+        <div className='login-section'>
+          <div className='form-heading'>
+            <img src={img1}/>
+          <h1>Hi! Welcome Back.</h1>
+        <p>Login to your account.</p>
+        </div>
+        <br></br>
+        <form>
+            <div className='details-div'>
+        <span className='icon'><FontAwesomeIcon icon={faEnvelope} /></span>
+        <input type='email' className='email' id='email' placeholder='Email Address'></input>
+            </div>
+
+            <div className='details-div'>
+        <span className='icon'><FontAwesomeIcon icon={faLock} /></span>
+        <input type='password' className='password' id='password' placeholder='Password'></input>
+            </div>
+            <p>
+        Don't have an account? <Link to="/signup" className="signup-link">Sign Up.</Link>
+      </p>
+            <button type='submit' className='btn-login'>Login</button>
+        </form>
+       
+        </div>
+        </>
+    )
+}
+
+export default Login
